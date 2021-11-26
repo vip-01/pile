0f2263529fdd06893075f7e008300a426f8a5a4c
vip-01 <vip_010821@163.com>
2021-11-26 23:49:17 +0800

    日常学习

---
+++ test_11_26/test_11_26/test.c
@@ -0,0 +1,51 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+
+//int main()
+//{
+//	int killer = 0;
+//	for (killer = 'a'; killer <= 'd'; killer++)
+//	{
+//		if ((killer != 'a') + (killer == 'c') + (killer == 'd') + (killer != 'd') == 3)
+//		{
+//			printf("keller = %c\n", killer);
+//		}
+//	}
+//	return 0;
+//}
+
+//五位运动员参加了10米跳水比赛，有人让他们预测比赛结果；
+int main()
+{
+	int a = 0;
+	int b = 0;
+	int c = 0;
+	int d = 0;
+	int e = 0;
+	for (a = 1; a <= 5; a++)
+	{
+		for (b = 1; b <= 5; b++)
+		{
+			for (c = 1; c <= 5; c++)
+			{
+				for (d = 1; d <= 5; d++)
+				{
+					for (e = 1; e <= 5; e++)
+					{
+						if (((b == 2) + (a == 3) == 1) &&
+							((b == 2) + (a == 4) == 1) &&
+							((c == 1) + (d == 2) == 1) &&
+							((c == 5) + (d == 3) == 1) &&
+							((c == 4) + (a == 1) == 1))
+						{
+							if (a*b*c*d*e == 120)
+								printf("a=%d b=%d c=%d d=%d e=%d\n", a, b, c, d, e);
+						}
+					}
+				}
+			}
+		}
+	}
+	return 0;
+}
