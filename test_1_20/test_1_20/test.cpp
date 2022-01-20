3b87a519a1a327f13bd220d537a350e286600d8d
vip-01 <vip_010821@163.com>
2022-01-20 23:54:06 +0800

    日常练习

---
+++ test_1_20/test_1_20/test.cpp
@@ -0,0 +1,105 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+//int main(int argc, const char *argv[]){
+//	for (int i = 10; i < 30; ++i){
+//		int i1 = i*i*i;
+//		int i2 = i1*i;
+//		if (i1 >= 1000 && i1 < 10000 && i2 >= 100000 && i2 < 1000000)
+//			cout << i << " " << i1 << " " << i2 << endl;
+//	}
+//	return 0;
+//}
+
+//int main(int argc, const char *argv[]){
+//	for (int i = 10; i < 30; ++i){
+//		int i1 = i*i*i;
+//		int i2 = i1*i;
+//		if (i1 >= 1000 && i1 < 10000 && i2 >= 100000 && i2 < 1000000)
+//			cout << i << " " << i1 << " " << i2 << endl;
+//	}
+//	return 0;
+//}
+
+//int main(int argc, const char *argv[]){
+//	for (int i = 10; i < 30; ++i){
+//		int i1 = i*i*i;
+//		int i2 = i1*i;
+//		if (i1 >= 1000 && i1 < 10000 && i2 >= 100000 && i2 < 1000000)
+//			cout << i << " " << i1 << " " << i2 << endl;
+//	}
+//	return 0;
+//}
+
+//int main(int argc, const char *argv[]){
+//	for (int i = 10; i < 30; ++i){
+//		int i1 = i*i*i;
+//		int i2 = i1*i;
+//		if (i1 >= 1000 && i1 < 10000 && i2 >= 100000 && i2 < 1000000)
+//			cout << i << " " << i1 << " " << i2 << endl;
+//	}
+//	return 0;
+//}
+
+//int main(int argc, char *argv[]){
+//	for (int i = 10; i < 30; ++i){
+//		int i1 = i*i*i;
+//		int i2 = i1*i;
+//		if (i1 >= 1000 && i1 < 10000 && i2 >= 100000 && i2 < 1000000)
+//			cout << i << " " << i1 << " " << i2 << endl;
+//	}
+//	return 0;
+//}
+
+//int main(int argc, char argv[]){
+//	for (int i = 10; i < 30; ++i){
+//		int i1 = i*i*i;
+//		int i2 = i1*i;
+//		if (i1 >= 1000 & i1 < 10000 && i2 >= 100000 && i2 < 1000000)
+//			cout << i << " " << i1 << " " << i2 << endl;
+//	}
+//	return 0;
+//}
+
+//int main(int argc, const char *argv){
+//	for (int i = 10; i < 30; ++i){
+//		int i1 = i*i*i;
+//		int i2 = i1*i;
+//		if (i1 >= 1000 && i1 < 10000 && i2 >= 100000 && i2 < 1000000)
+//			cout << i << " " << i1 << " " << i2 << endl;
+//	}
+//	return 0;
+//}
+
+//int main(int argc, const char *argv[]){
+//	for (int i = 10; i < 30; ++i){
+//		int i1 = i*i*i;
+//		int i2 = i1*i;
+//		if (i1 >= 1000 && i1 < 10000 && i2 >= 100000 && i2 < 1000000)
+//			cout << i << " " << i1 << " " << i2 << endl;
+//	}
+//	return 0;
+//}
+
+//int main(int argc, char *argv[]){
+//	for (int i = 10; i < 30; ++i){
+//		int i1 = i*i*i;
+//		int i2 = i1*i;
+//		if (i1 >= 1000 && i1 < 10000 && i2 >= 100000 && i2 < 1000000)
+//			cout << i << " " << i1 << " " << i2 << endl;
+//	}
+//	return 0;
+//}
+
+int main(int argc, const char *argv[]){
+	for (int i = 10; i < 30; ++i){
+		int i1 = i*i*i;
+		int i2 = i1*i;
+		if (i1 >= 1000 && i1 < 10000 && i2 >= 100000 && i2 < 1000000)
+			cout << i << " " << i1 << " " << i2 << endl;
+	}
+	return 0;
+}
